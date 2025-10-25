88ac0dfecd08b68bb0cddad8bc5d40e5988ff6b3
Prakasa <prakasa@devetek.com>
2025-10-25 16:14:07 +0700

    feat: deploy filebrowser to VPS with docker

---
+++ filebrowser/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Building docker-example...."
+echo "You can use build command to clean up unused docker image related with your project."
+echo "You can use build script to preparing any environment for your project. Such as create directory, install dependency, etc."
+
+docker pull filebrowser/filebrowser
---
+++ filebrowser/start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# create network if it is not exist
+docker network create dpanel-network || true
+
+# exit container if it is running
+docker rm ${DPANEL_SYSTEM_APP_NAME}-${DPANEL_SYSTEM_LINUX_USER} || true
+
+# Start the container, and map port ${PORT} to port 80 in the container.
+
+docker run \
+    --network dpanel-network \
+    --volume filebrowser_data:/srv \
+    --volume filebrowser_database:/database \
+    --volume filebrowser_config:/config \
+    --publish ${PORT}:80 \
+    --network-alias ${DPANEL_SYSTEM_APP_NAME}-${DPANEL_SYSTEM_LINUX_USER} \
+    --name ${DPANEL_SYSTEM_APP_NAME}-${DPANEL_SYSTEM_LINUX_USER} \
+    --hostname ${DPANEL_SYSTEM_APP_NAME}-${DPANEL_SYSTEM_LINUX_USER} \
+    filebrowser/filebrowser

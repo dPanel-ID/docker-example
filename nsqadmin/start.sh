db17fb3871d160fdcd27dbfcda75415d10a0847e
Prakasa <prakasa@devetek.com>
2026-01-03 09:21:49 +0700

    feat: add nsqadmin deployment

---
+++ nsqadmin/start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# remove container, make sure clean start
+docker rm ${DPANEL_SYSTEM_APP_NAME}-${DPANEL_SYSTEM_LINUX_USER} || true
+
+# Start the main container, and map port ${PORT} to port in the container.
+docker run \
+    --network dpanel-network \
+    --restart=unless-stopped \
+    --user $(id -u):$(id -g) \
+    --publish 4171:4171 \
+    --network-alias ${DPANEL_SYSTEM_APP_NAME}-${DPANEL_SYSTEM_LINUX_USER} \
+    --name ${DPANEL_SYSTEM_APP_NAME}-${DPANEL_SYSTEM_LINUX_USER} \
+    --hostname ${DPANEL_SYSTEM_APP_NAME}-${DPANEL_SYSTEM_LINUX_USER} \
+    nsqio/nsq:v1.3.0 \
+    /nsqadmin --lookupd-http-address=${NSQLOOKUPD_ADDRESS}
